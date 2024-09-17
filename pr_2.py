9c8f94ee1b1e7eaaf943e1fc0eb45c96bf906c96
denyss <d.pnsw4444@gmail.com>
2024-09-18 01:04:59 +0300

    Add files via upload

---
+++ pr_2.py
@@ -0,0 +1,23 @@
+hello = "Hello, World!"
+name = "Denis"
+last_name = "Panasiyk"
+year = 16
+
+print(type (hello), type (name), type (last_name), type (year)) 
+
+type_lst = (type (hello), type (name), type (last_name), type (year)) 
+
+values = [ hello, name, last_name, year]
+
+types = [type(value) for value in values]
+
+type_count = {}
+for t in types :
+    if t in type_count:
+        type_count[t] += 1
+    else:
+        type_count[t] = 1
+
+most_common_type = max(type_count, key=type_count.get)
+
+print(f"Найчастіше зустрічається тип даних: {most_common_type.__name__}, кількість повторень: {type_count[most_common_type]}")
